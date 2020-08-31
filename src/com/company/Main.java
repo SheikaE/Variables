e5df5db99a7d5292dc96a9cc8360eb6b7b841826
Sheika Etienne <sheikaetienne@Sheikas-MacBook-Pro.local>
2020-08-31 16:23:41 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	int test_score = 50;
+	float price_tag = 24.99f;
+	String user_name = ("Bobby Johnson");
+	System.out.println("Test Score initalized is:" + test_score);
+	System.out.println("Price tag: " + price_tag);
+	System.out.println("Username: " + user_name);
+
+
+
+    }
+}
